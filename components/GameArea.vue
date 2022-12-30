e32872f17aafda504a09abebddf2664223facb42
Vanessi-K <vanessa.koeck.26@outlook.com>
2022-12-30 11:12:39 +0100

    get a random word from the api

--- components/GameArea.vue
+++ components/GameArea.vue
@@ -1,5 +1,6 @@
 <template>
   <ClientOnly>
+    {{randomWord}}
     <ButtonChoseFont v-for="guessOption in guessOptions" :font="guessOption"></ButtonChoseFont>
   </ClientOnly>
 </template>
@@ -7,6 +8,16 @@
 <script>
 export default {
   name: "GameArea",
+  data() {
+    return {
+      randomWord: ""
+    }
+  },
+  async mounted() {
+    const {data} = await useFetch('https://random-word-api.herokuapp.com/word')
+    this.randomWord = data;
+    this.randomWord = this.randomWord[0]
+  },
   computed: {
     guessOptions() {
       let optionsArray = this.optionFonts;
